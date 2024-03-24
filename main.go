da40f5de9cb9c50b5ca6d6f1b869a95535a322f2
fde-capu <flavio@carrara.us>
2024-03-24 17:03:39 -0300

    Downgraded to go 1.19 (current Debian stable). godotenv had to be imported.

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
 import "github.com/nitintf/navi/cmd"
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	cmd.Execute()
 }
