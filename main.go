0fe63efe981675d3d9ab41aec13af3b4f75be163
Flávio Carrara <flavio@carrara.us>
2024-03-25 11:03:41 -0300

    Make the executable read .env files properly. The .env must be on the same directory that the binary is.

--- main.go
+++ main.go
@@ -3,5 +3,25 @@ package main
 import "github.com/nitintf/navi/cmd"
 
 func main() {
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	exePath, err := os.Executable()
+	if err != nil {
+		log.Fatalf("Error getting current exectuable path: ", err)
+	}
+	exeDir := filepath.Dir(exePath)
+	envPath := filepath.Join(exeDir, ".env")
+	err = godotenv.Load(envPath)
+	if err != nil {
+		log.Fatal("Error loading .env file: ", err)
+	}
+
 	cmd.Execute()
 }
